e45b1cf25b6e98c881f7d47591563e00a0aa43a7
USER <hblee@progist.co.kr>
2026-06-19 16:07:09 +0900

    1

---
+++ src/main/kotlin/com/nodex/allcrew/AllcrewApplication.kt
@@ -0,0 +1,11 @@
+package com.nodex.allcrew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllcrewApplication
+
+fun main(args: Array<String>) {
+    runApplication<AllcrewApplication>(*args)
+}
---
+++ src/test/kotlin/com/nodex/allcrew/AllcrewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nodex.allcrew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllcrewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
